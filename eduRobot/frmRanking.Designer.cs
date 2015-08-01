75b100e3e866bdb806e20209c173641698658aa1
chuongntv <chuongntv.1409@gmail.com>
2015-08-01 16:16:56 +0700

    Update About Design and Code for Ranking Form

--- eduRobot/frmRanking.Designer.cs
+++ eduRobot/frmRanking.Designer.cs
@@ -28,22 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.cbLevel = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.listView1 = new System.Windows.Forms.ListView();
-            this.button2 = new System.Windows.Forms.Button();
+            this.lvRanking = new System.Windows.Forms.ListView();
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // comboBox1
+            // cbLevel
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(79, 12);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(145, 21);
-            this.comboBox1.TabIndex = 0;
+            this.cbLevel.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbLevel.FormattingEnabled = true;
+            this.cbLevel.Location = new System.Drawing.Point(79, 12);
+            this.cbLevel.Name = "cbLevel";
+            this.cbLevel.Size = new System.Drawing.Size(194, 21);
+            this.cbLevel.TabIndex = 0;
+            this.cbLevel.SelectedIndexChanged += new System.EventHandler(this.cbLevel_SelectedIndexChanged);
             // 
             // label1
             // 
@@ -54,30 +57,21 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "Ranking by";
             // 
-            // listView1
+            // lvRanking
             // 
-            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.lvRanking.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeader1,
             this.columnHeader2,
-            this.columnHeader3});
-            this.listView1.FullRowSelect = true;
-            this.listView1.GridLines = true;
-            this.listView1.Location = new System.Drawing.Point(12, 39);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(293, 208);
-            this.listView1.TabIndex = 2;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.View = System.Windows.Forms.View.Details;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(230, 10);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "Close";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.columnHeader3,
+            this.columnHeader4});
+            this.lvRanking.FullRowSelect = true;
+            this.lvRanking.GridLines = true;
+            this.lvRanking.Location = new System.Drawing.Point(12, 39);
+            this.lvRanking.Name = "lvRanking";
+            this.lvRanking.Size = new System.Drawing.Size(342, 208);
+            this.lvRanking.TabIndex = 2;
+            this.lvRanking.UseCompatibleStateImageBehavior = false;
+            this.lvRanking.View = System.Windows.Forms.View.Details;
             // 
             // columnHeader1
             // 
@@ -87,27 +81,43 @@
             // columnHeader2
             // 
             this.columnHeader2.Text = "Player\'s Nickname";
-            this.columnHeader2.Width = 200;
+            this.columnHeader2.Width = 160;
             // 
             // columnHeader3
             // 
             this.columnHeader3.Text = "Point";
             // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Time";
+            this.columnHeader4.Width = 90;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(279, 10);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Close";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // frmRanking
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(315, 258);
+            this.ClientSize = new System.Drawing.Size(366, 258);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.lvRanking);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.cbLevel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "frmRanking";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Ranking";
+            this.Load += new System.EventHandler(this.frmRanking_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -115,12 +125,13 @@
 
         #endregion
 
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox cbLevel;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView lvRanking;
         private System.Windows.Forms.ColumnHeader columnHeader1;
         private System.Windows.Forms.ColumnHeader columnHeader2;
         private System.Windows.Forms.ColumnHeader columnHeader3;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
     }
 }
--- eduRobot/frmRanking.cs
+++ eduRobot/frmRanking.cs
@@ -7,19 +7,87 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Data.OleDb;
+using System.Data;
 
 namespace eduRobot
 {
     public partial class frmRanking : Form
     {
+        private OleDbConnection conn;
+        private OleDbDataReader reader;
+        private OleDbCommand cmd;
+        private string sqlStr;
         public frmRanking()
         {
+            conn = new OleDbConnection(Program.conStr);
             InitializeComponent();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void frmRanking_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                int i = 1;
+                while (true)
+                {
+                    if (!File.Exists("Map\\" + i + ".map"))
+                        break;
+                    else
+                    {
+                        cbLevel.Items.Add("Level " + i++);
+                    }
+
+                }
+                cbLevel.SelectedIndex = 0;
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void cbLevel_SelectedIndexChanged(object sender, EventArgs e)
+        {   
+            try
+            {
+                lvRanking.Items.Clear();
+                sqlStr = "SELECT TOP 10 * FROM rankinglogs WHERE levelnumber = " + cbLevel.Text.Substring(6).Trim() + "   ORDER BY Point";
+                conn.Open();
+                cmd = new OleDbCommand(sqlStr,conn);
+                reader = cmd.ExecuteReader();
+                int i = 1;
+                while(reader.Read())
+                {
+                    ListViewItem lvi = new ListViewItem(i+"");
+                    lvi.SubItems.Add(reader["Nickname"].ToString());
+                    lvi.SubItems.Add(reader["Point"].ToString());
+                    lvi.SubItems.Add(reader["TimeSave"].ToString());
+                    lvRanking.Items.Add(lvi);
+                    i++;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }     
+            finally
+            {
+                try
+                {
+                    conn.Close();
+                }
+                catch
+                {
+                }
+            }
+           
         }
     }
 }
